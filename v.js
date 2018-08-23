85f32dd24601b46748c3909775edc8d0fd261881
qiaokun199 <41895921+qiaokun199@users.noreply.github.com>
2018-08-23 10:01:22 +0800

    vue

---
+++ v.js
@@ -0,0 +1,77 @@
+
+var v= new Vue({
+  el:"#app",
+   data:{
+    objects:[{     
+      "src":"2.png",
+      "username":"Chris Peng",
+      "action_a":"add",
+      "object_a":"Note",
+      "define":"1/29/2018 Spring Summary",
+      "m_body":"Developers make sure that finish all new feature development and critical issue fixing and merge all code to master branch at this...",
+      "time_a":"07:31 PM",
+      "src1":"7.png",
+      "seen":true,
+      "none":false
+      },
+      {     
+      "src":"3.png",
+      "username":"Chris Peng",
+      "action_a":"updated",
+      "object_a":"Meeting",
+      "define":"KKKKK",
+      "m_body":"status on 2018/03/09 11:00 AM and assign to Outlook Ying,ting ma",
+      "time_a":"07:31PM",
+      "src1":"7.png",
+      "seen":true,
+      "none":false
+      },
+      {     
+      "src":"4.png",
+      "username":"Chris Peng",
+      "action_a":"updated",
+      "object_a":"Document",
+      "define":"KKKKK.jpg",
+      "time_a":"07:31PM",
+      "src1":"7.png",
+      "src2":"w.png",
+      "seen":false,
+      "none":true 
+      },
+    
+    {     
+      src:"5.png",
+      "username":"Chris Peng",
+      "action_a":"received",
+      "object_a":"an email to",
+      "define2":"allen@yhlsoft.com",
+      "m_body":"Now US Release Plan-staging need support people to help testing Added new showstopper issuses for staging,the big issue is that many...",
+      "time_a":"07:31PM",
+      "src1":"0.png",
+      "seen":true,
+      "none":false 
+      },
+      {
+      "src":"6.png",
+      "username":"Chris Peng",
+      "action_a":"sent",
+      "object_a":"an email to",
+      "define2":"allen@yhlsoft.com",
+      "m_body":"Now US Release Plan-staging need support people to help testing Added new showstopper issuses for staging,the big issue is that many...",
+      "time_a":"07:31PM",
+      "seen":true,
+      "none":false
+      },
+      {
+      "src":"6.png",
+      "username":"Chris Peng",
+      "action_a":"sent",
+      "object_a":"an email to",
+      "define2":"allen@yhlsoft.com",
+      "m_body":"Now US Release Plan-staging need support people to help testing Added new showstopper issuses for staging,the big issue is that many...",
+      "time_a":"07:31PM"
+      }
+      ]
+      
+    }});
+    
